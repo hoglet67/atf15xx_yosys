493f6572820d58f3bb43a5a3a8ee2abc71dff6b8
David Banks <dave@hoglet.com>
2022-11-23 17:12:36 +0000

    run_fitter: better summary at end, split command across multiple lines

--- run_fitter.sh
+++ run_fitter.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
 export ROOT=$(dirname $0)
 export WINEPREFIX=$HOME/.wine_atf
@@ -13,6 +13,46 @@ OPTS=$(grep '//OPT:' ${NAME}.v | cut -d' ' -f2-)
 
 grep '//PIN:' ${NAME}.v | cut -d' ' -f2- | tee ${NAME}.pin
 
-wine ${FITTERDIR}\\fit${DEVICE}.exe -i ${NAME}.edif -o ${NAME}.jed -device ${PACKAGE} -tech ATF${DEVICE}AS -strategy ifmt=edif -strategy optimize=on -strategy DEBUG=off -strategy Verilog_sim Verilog ${OPTS} -strategy xor_synthesis on
 
-tail -25 ${NAME}.fit
+## Atmel foldback = Altera shareable expander
+## Atmel cascade = Altera parallel expander
+
+
+rm -f ${NAME}.fit
+
+wine ${FITTERDIR}\\fit${DEVICE}.exe \
+     -i ${NAME}.edif \
+     -ifmt edif \
+     -o ${NAME}.jed \
+     -device ${PACKAGE} \
+     -tech ATF${DEVICE}AS \
+     -preassign keep \
+     -strategy jtag on \
+     -strategy tdi_pullup on \
+     -strategy tms_pullup on \
+     -strategy output_fast off \
+     -strategy pin_keep off \
+     -strategy pd1 off \
+     -strategy pd2 off \
+     -strategy optimize on \
+     -strategy Verilog_sim Verilog \
+     -strategy cascade_logic off \
+     -strategy foldback_logic on \
+     -strategy xor_synthesis on \
+     ${OPTS}
+
+grep "^   " ${NAME}.fit
+echo
+
+echo "Logic Array Block       Macro Cells     I/O Pins        Foldbacks       TotalPT         FanIN           Cascades"
+grep "^[A-H]:" ${NAME}.fit
+echo
+
+grep "FanIn"   ${NAME}.fit
+echo
+
+grep "^Total"  ${NAME}.fit | sed "s/input pins \t/input pins/" | sed "s/Pts \t/Pts/"
+echo
+
+grep "^\$Device"  ${NAME}.fit
+echo
