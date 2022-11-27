8adc1218cbffb120def45f59df81396a6984646f
David Banks <dave@hoglet.com>
2022-11-27 15:57:27 +0000

    mmu example: pass params through build script

--- examples/mmu/build.sh
+++ examples/mmu/build.sh
@@ -2,4 +2,6 @@
 
 ../../run_yosys.sh mmu > mmu.log
 
-../../run_fitter.sh mmu -preassign keep -tdi_pullup on -tms_pullup on -output_fast off -foldback_logic on -xor_synthesis on
+echo $*
+
+../../run_fitter.sh mmu -preassign keep -tdi_pullup on -tms_pullup on -output_fast off -xor_synthesis on $*
